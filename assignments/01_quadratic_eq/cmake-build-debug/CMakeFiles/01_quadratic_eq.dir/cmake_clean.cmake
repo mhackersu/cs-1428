d20e5f0634de43afc62a58460361d0aceb810dd1
Michael Neal <iammichaelneal@gmail.com>
2018-09-17 22:09:28 -0500

    assignment 01

---
+++ assignments/01_quadratic_eq/cmake-build-debug/CMakeFiles/01_quadratic_eq.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/01_quadratic_eq.dir/main.cpp.o"
+  "01_quadratic_eq.pdb"
+  "01_quadratic_eq"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/01_quadratic_eq.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ assignments/01_quadratic_eq/cmake-build-debug/CMakeFiles/01_quadratic_eq.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 01_quadratic_eq.
+# This may be replaced when dependencies are built.
