29c08648a19d67aeef7ac47f30a3af7e3d296ab9
Michael Neal <iammichaelneal@gmail.com>
2018-09-20 10:09:06 -0500

    assignment 01

--- assignments/01_quadratic_eq/main.cpp
+++ assignments/01_quadratic_eq/main.cpp
@@ -4,10 +4,10 @@ using namespace std;
 
 int main() {
 
-    float a, b, c, result1, result2, discrim;
+    float a, b, c, d, d1, d2, result1, result2;
 
-    cout << "Welcome to the quadratic equation solver drive thru machine. Please enter the inputs."
-            " Note: This machine does not currently support complex or imaginary roots." << endl;
+    cout << "Welcome to the quadratic equation calculator. Please enter the inputs." << endl;
+    cout << "Note: This machine does not support complex or imaginary roots." << endl;
 
     cout << "Enter the coefficient for a: " << endl;
     cin >> a;
@@ -16,17 +16,27 @@ int main() {
     cout << "Enter the coefficient for c: " << endl;
     cin >> c;
 
-    discrim = ( b*b ) - ( 4*a*c ); // Calculate the discriminant.
+    // Calculate the discriminant using the pow function.
+    d = (( pow (b, 2) ) - ( 4*a*c ));
 
-    if (discrim > 0 || discrim == 0) {
-        result1 = (-b + sqrt(discrim)) / ( 2*a );
-        result2 = (-b - sqrt(discrim)) / ( 2*a );
+    // Calculate the roots using the pow function.
+    d1 = (-b + ( pow ( d, (1/2) )));
+    d2 = (+b + ( pow ( d, (1/2) )));
+
+    if (d > 0 || d == 0) {
+        result1 = d1 / ( 2*a );
+        result2 = d2 / ( 2*a );
         cout << "The roots of x are: " << result1 << " , " << result2 << endl;
+    } else {
+        cout << "The roots are complex and or imaginary and are not supported by this calculator." << endl;
     }
 
-    else {
-        cout << "The roots are complex and are not supported by this machine." << endl;
-    }
+    // Testing outputs
+
+    // cout << "Testing outputs: " << endl;
+    // cout << "The output of d is: " << d << endl;
+    // cout << "The output of d1 is: " << d1 << endl;
+    // cout << "The output of d2 is: " << d2 << endl;
 
     return 0;
 }
